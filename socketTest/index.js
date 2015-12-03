29dc0c578e069f841296d4abd7348f0eb63fee4d
Om Mahida <ommahida@gmail.com>
2015-12-02 23:58:36 -0500

    bash script runs from server locally

--- socketTest/index.js
+++ socketTest/index.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var spawn = require('child_process').spawn;
+var _ = require('underscore'); //for utility goodness
 
 
 app.get('/', function(req, res){
@@ -12,12 +14,21 @@ io.on('connection', function(socket){
 		io.emit('chat message', msg);
 
 		if(msg=="hi"){
-			io.emit('chat message', 'D.O.D.: Hi! How are you? Jk I do not really care');
-			console.log("user sent hi");
-			io.emit('chat message', 'Now try: Move your tail');
+			io.emit('chat message', 'D.O.D.:  Hi! How are you? Jk I do not really care');
+			console.log("user sent hi to me");
+			io.emit('chat message', 'Now try, "fly"');
 
 			console.log(`ls`);
 
+		} else if (msg == "fly") {
+			io.emit('chat message', 'D.O.D.:  I am trying my best!');
+			console.log("flapping my wings...");
+
+
+			var deploySh = spawn('sh', [ 'helloWorld' ], {
+				cwd: process.env.HOME + '/Documents/dragon/socketTest/',
+				env:_.extend(process.env, { PATH: process.env.PATH + ':/usr/local/bin'})
+			});
 		}
 
 
